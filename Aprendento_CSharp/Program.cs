8b74d49635c2a6b18884986370dd871a2990ac6e
Lucas Diogenes <lucasdiogenes_dev@hotmail.com>
2024-03-15 16:41:20 -0300

    Novo projeto

---
+++ Aprendento_CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aprendento_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
